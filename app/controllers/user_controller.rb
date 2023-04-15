dc2f653829ddb3dde8e302fe540081b405130d4f
pvnamGit <pvnam.forwork@gmail.com>
2023-04-15 10:25:14 +0700

    feat: register view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,23 +1,27 @@
 class UserController < ApplicationController
 
   def new
-    @user = User.new
+    user = User.new
+  end
+
+  def index
   end
 
   def create
     if (user_params[:password] != user_params[:password_confirmation])
       return flash[:alert] = "Wrong confirmation password"
     end
-    
+
     @user = User.new(user_params)
     if (@user.save)
       session[:current_user_id] = @user.id
       flash[:notice] = "User created."
+      redirect_to root_path
     end
   end
 
-    private
-    def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation)
-    end
+  private
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
